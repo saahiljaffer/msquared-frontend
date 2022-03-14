f2db00580f3ef3c8fd51e39f18806ee512667ac2
Saahil Jaffer <saahiljaffer@ritual.co>
2022-03-14 10:12:13 -0400

    Makes nav a link

--- src/pages/PageWithNav/index.jsx
+++ src/pages/PageWithNav/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Nav from "../../components/Nav/Nav";
 import { H2 } from "../../components/Fonts/Secondary";
 import PageContainer from "../../components/PageContainer/PageContainer";
@@ -12,17 +12,12 @@ const MainTitle = styled(H2)`
 `;
 
 function PageWithNav({ children }) {
-  const navigate = useNavigate;
   return (
     <>
       <Nav>
-        <MainTitle
-          onClick={() => {
-            navigate.push("/");
-          }}
-        >
-          Maysum and Malika
-        </MainTitle>
+        <Link to="/">
+          <MainTitle>Maysum and Malika</MainTitle>
+        </Link>
       </Nav>
       <PageContainer>{children}</PageContainer>
     </>
